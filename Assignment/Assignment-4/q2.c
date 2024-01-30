7a97f96879ec9da554a3849e84d14467e3b2ae71
Md Ibrahim <faiziibrahim321@gmail.com>
2024-01-30 21:26:56 +0530

    q2

---
+++ Assignment/Assignment-4/q2.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int fib(int n){
+ if (n==0)
+ 
+    return 0;
+ 
+   if (n==1)
+   
+     return 1;
+   
+   
+ 
+
+
+    return fib(n-1)+fib(n-2);
+ 
+  
+   
+
+}
+int main(){
+
+    int n;
+    scanf("%d",&n);
+    int a=fib(n);
+
+    printf("%d",a);
+ 
+    
+
+
+
+    return 0;
+}
