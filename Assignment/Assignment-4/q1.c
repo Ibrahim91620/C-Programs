819e17ef0cf7d28f3d2e3fda9a2c7a870e7e540e
Md Ibrahim <faiziibrahim321@gmail.com>
2024-01-30 21:22:43 +0530

    q1

---
+++ Assignment/Assignment-4/q1.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int fact(int n){
+ 
+   if (n==0)
+   {
+    return 1;
+   }
+   else{
+
+    return n*fact(n-1);
+   }
+   
+
+}
+int main(){
+
+    int n;
+    scanf("%d",&n);
+    int a=fact(n);
+
+    printf("%d",a);
+ 
+    
+
+
+
+    return 0;
+}
