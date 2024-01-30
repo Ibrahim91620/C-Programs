9e8666663d229cd0ecc591ca13a4edfa8b3eb59b
Md Ibrahim <faiziibrahim321@gmail.com>
2024-01-30 19:48:30 +0530

    q3.c

---
+++ Assignment/Assignment-1/q3.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+
+ int n;
+
+ scanf("%d",&n);
+
+ if(n%2==0){
+    printf("It is even Number");
+
+ }
+ else{
+    printf("It is odd number");
+    
+ }
+
+
+    return 0;
+}
